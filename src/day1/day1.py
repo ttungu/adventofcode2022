d0185643d87cb5a1822928538820f9eaa3c55fd3
Tung Tran <tomas.tran95@gmail.com>
2022-12-04 14:47:26 +0100

    ADD refactor DAY1

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -1,17 +1,12 @@
 # https://adventofcode.com/2022/day/1
-tmp = []
-with open("src/day1/input.txt", "r") as f:
-    for lines in f:
-        tmp.append(lines)
-tmp.append('\n')
-sum1= 0
-help = []
-for t in tmp:
-    if(t != "\n"):
-        sum1 = sum1 + int(t)
-    elif('\n'):
-        
-        help.append(sum1)
-        sum1=0
-help.sort(reverse=True)
-print(help[0] + help[1] + help[2])
+allCalories = []
+input = [l.strip() for l in open("src/day1/input.txt")]
+for elf in ('\n'.join(input).split('\n\n')):
+    sumCalories = 0
+    for calorie in elf.split('\n'):
+        sumCalories += int(calorie)
+    allCalories.append(sumCalories)
+    
+allCalories.sort(reverse=True)
+print(allCalories[0])
+print(allCalories[0] + allCalories[1] + allCalories[2])
